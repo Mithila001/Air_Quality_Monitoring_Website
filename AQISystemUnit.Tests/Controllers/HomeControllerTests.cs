5eeff992702cffe1b90db68d4c783c914e221291
dananjaya2002 <kavishandananjaya2002@gmail.com>
2025-04-28 22:19:06 +0530

    new fix of test files

--- AQISystemUnit.Tests/Controllers/HomeControllerTests.cs
+++ AQISystemUnit.Tests/Controllers/HomeControllerTests.cs
@@ -1,15 +1,17 @@
 ﻿using Xunit;
 using Moq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.InMemory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using SDTP_Project1.Controllers;
 using SDTP_Project1.Data;
 using SDTP_Project1.Models;
+using SDTP_Project1.Services;
 using Microsoft.AspNetCore.Mvc;
 using FluentAssertions;
-
+using Microsoft.AspNetCore.Http;
 
 namespace AQISystemUnit.Tests.Controllers
 {
@@ -50,7 +52,6 @@ namespace AQISystemUnit.Tests.Controllers
                 Description = string.Empty
             };
             context.Sensors.AddRange(sensor1, sensor2);
-
             var readings = new List<AirQualityData>
             {
                 new AirQualityData { MeasurementID = 1, SensorID = "S1", Timestamp = DateTime.UtcNow.AddMinutes(-1), AQI = 50, Sensor = sensor1 },
@@ -60,7 +61,10 @@ namespace AQISystemUnit.Tests.Controllers
             context.AirQualityData.AddRange(readings);
             await context.SaveChangesAsync();
 
-            var controller = new HomeController(context);
+            // Mock the ISensorService
+            var mockSensorService = new Mock<ISensorService>();
+
+            var controller = new HomeController(context, mockSensorService.Object);
 
             // Act
             var result = await controller.Index();
@@ -68,7 +72,6 @@ namespace AQISystemUnit.Tests.Controllers
             // Assert: should be ViewResult with model
             var viewResult = result.Should().BeOfType<ViewResult>().Subject;
             viewResult.ViewData.Model.Should().BeAssignableTo<List<SensorDataViewModel>>();
-
             var model = viewResult.ViewData.Model as List<SensorDataViewModel>;
             model.Should().HaveCount(1, "only active sensors should be included");
             var sensorData = model.First();
@@ -80,9 +83,10 @@ namespace AQISystemUnit.Tests.Controllers
         [Fact]
         public void TestTailwind_ShouldReturnViewResult()
         {
-            // Arrange: mock context just to satisfy constructor
+            // Arrange: mock context and service to satisfy constructor
             var mockContext = new Mock<AirQualityDbContext>(new DbContextOptions<AirQualityDbContext>());
-            var controller = new HomeController(mockContext.Object);
+            var mockSensorService = new Mock<ISensorService>();
+            var controller = new HomeController(mockContext.Object, mockSensorService.Object);
 
             // Act
             var result = controller.TestTailwind();
@@ -90,6 +94,73 @@ namespace AQISystemUnit.Tests.Controllers
             // Assert
             result.Should().BeOfType<ViewResult>();
         }
+
+        [Fact]
+        public void Error_ShouldReturnViewResult_WithErrorViewModel()
+        {
+            // Arrange
+            var mockContext = new Mock<AirQualityDbContext>(new DbContextOptions<AirQualityDbContext>());
+            var mockSensorService = new Mock<ISensorService>();
+            var controller = new HomeController(mockContext.Object, mockSensorService.Object);
+
+            // Setup HttpContext to have a TraceIdentifier
+            var httpContext = new DefaultHttpContext();
+            httpContext.TraceIdentifier = "test-trace-id";
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var result = controller.Error(404);
+
+            // Assert
+            var viewResult = result.Should().BeOfType<ViewResult>().Subject;
+            viewResult.ViewName.Should().Be("Error");
+            var model = viewResult.Model.Should().BeOfType<ErrorViewModel>().Subject;
+            model.RequestId.Should().Be("test-trace-id");
+        }
+
+        [Fact]
+        public async Task SensorDetails_ShouldReturnPartialView_WithReadings()
+        {
+            // Arrange
+            var mockContext = new Mock<AirQualityDbContext>(new DbContextOptions<AirQualityDbContext>());
+            var mockSensorService = new Mock<ISensorService>();
+            var controller = new HomeController(mockContext.Object, mockSensorService.Object);
+
+            var sensorId = "S1";
+            var fakeReadings = new List<AirQualityData>
+            {
+                new AirQualityData { MeasurementID = 1, SensorID = sensorId, Timestamp = DateTime.UtcNow }
+            };
+
+            mockSensorService
+                .Setup(s => s.GetLatestReadingsAsync(sensorId, 500))
+                .ReturnsAsync(fakeReadings);
+
+            // Act
+            var result = await controller.SensorDetails(sensorId);
+
+            // Assert
+            var partialViewResult = result.Should().BeOfType<PartialViewResult>().Subject;
+            partialViewResult.ViewName.Should().Be("_SensorDetailsPartial");
+            partialViewResult.Model.Should().BeSameAs(fakeReadings);
+        }
+
+        [Fact]
+        public async Task SensorDetails_WithEmptySensorId_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var mockContext = new Mock<AirQualityDbContext>(new DbContextOptions<AirQualityDbContext>());
+            var mockSensorService = new Mock<ISensorService>();
+            var controller = new HomeController(mockContext.Object, mockSensorService.Object);
+
+            // Act
+            var result = await controller.SensorDetails(string.Empty);
+
+            // Assert
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
     }
 }
-
